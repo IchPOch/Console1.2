0ea4b35c44c79721f50181e75f6d24df7f4a2a5c
Миша Епринцев <meprincev@bk.ru>
2020-05-04 00:53:55 +0300

    123

---
+++ 1.2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ch, x, y;
+            Console.WriteLine("Введите трёхзначное число");
+            ch = Convert.ToInt32(Console.ReadLine());
+            if (ch < 100 || ch > 999) Console.WriteLine("Введённое число не трёхзнач");
+            else
+            {
+                x = ch / 100;
+                y = ch % 10;
+                if (x > y)
+                    Console.WriteLine("Первое больше");
+                else
+                    Console.WriteLine("Последнее больше");
+            }
+        }
+    }
+
+}
